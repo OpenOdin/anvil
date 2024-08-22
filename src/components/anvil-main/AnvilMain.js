327a5ed059e04d20ba9473c580c822f593c75c2b
Thomas Backlund <bashlund@users.noreply.github.com>
2024-08-22 16:21:55 +0200

    Remove compiled .js files

--- src/components/anvil-main/AnvilMain.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AnvilMain = void 0;
-const riotjs_simple_typescript_1 = require("riotjs-simple-typescript");
-class AnvilMain extends riotjs_simple_typescript_1.Base {
-}
-exports.AnvilMain = AnvilMain;
