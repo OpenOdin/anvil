2fa9ecd7da8e13413deccb7ef391460fb5422124
Thomas Backlund <bashlund@users.noreply.github.com>
2024-08-22 13:48:38 +0200

    Update src/includes.js

--- src/includes.js
+++ src/includes.js
@@ -9,6 +9,9 @@ riot.register("raw", Raw);
 import Anvil404 from "./components/anvil-404/anvil-404.riot";
 riot.register("anvil-404", Anvil404);
 
+import AnvilWaiting from "./components/anvil-waiting/anvil-waiting.riot";
+riot.register("anvil-waiting", AnvilWaiting);
+
 import AnvilMain from "./components/anvil-main/anvil-main.riot";
 riot.register("anvil-main", AnvilMain);
 
@@ -23,6 +26,21 @@ riot.register("anvil-auth-2", AnvilAuth2);
 
 import AnvilAuth3 from "./components/anvil-auth/anvil-auth-3/anvil-auth-3.riot";
 riot.register("anvil-auth-3", AnvilAuth3);
+
+import AnvilData from "./components/anvil-data/anvil-data.riot";
+riot.register("anvil-data", AnvilData);
+
+import AnvilTab from "./components/anvil-tab/anvil-tab.riot";
+riot.register("anvil-tab", AnvilTab);
+
+import AnvilThread from "./components/anvil-thread/anvil-thread.riot";
+riot.register("anvil-thread", AnvilThread);
+
+import AnvilThreadConfigure from "./components/anvil-thread/anvil-thread-configure/anvil-thread-configure.riot";
+riot.register("anvil-thread-configure", AnvilThreadConfigure);
+
+import AnvilThreadTabular from "./components/anvil-thread/anvil-thread-tabular/anvil-thread-tabular.riot";
+riot.register("anvil-thread-tabular", AnvilThreadTabular);
 
 import "./index.css";
 
