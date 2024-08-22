3dd91c73111261fc6d8aca2d66324f42e58ed2f0
Thomas Backlund <bashlund@users.noreply.github.com>
2024-08-22 13:44:15 +0200

    Add anvil-data component

---
+++ src/components/anvil-data/AnvilData.ts
@@ -0,0 +1,49 @@
+import {
+    Base,
+} from "riotjs-simple-typescript";
+
+import {
+    Service,
+} from "openodin";
+
+import {
+    AnvilThreadController,
+} from "../../lib/AnvilThreadController";
+
+type Thread = {
+    id: string,
+    name: string,
+};
+
+export interface AnvilDataProps {
+    service: Service;
+}
+
+export interface AnvilDataState {
+    tabId: number;
+    threads: AnvilThreadController[];
+}
+
+export class AnvilData extends Base<AnvilDataProps, AnvilDataState> {
+    protected threadId: number = 1;
+
+    public onBeforeMount(props: AnvilDataProps, state: AnvilDataState) {
+        state.tabId = 0;
+
+        const name = "presence";
+
+        const threadTemplate = props.service.getThreadTemplates()[name];
+
+        state.threads = [
+            new AnvilThreadController(props.service, this.threadId++, name, threadTemplate),
+        ];
+
+        // TODO hook service onClose kill all threads
+    }
+
+    public tabClicked = (id: number) => {
+        this.state.tabId = id;
+
+        this.update();
+    }
+}
